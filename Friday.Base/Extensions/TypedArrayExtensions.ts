d9eb92e41e15ee6615d4563dad727cf7840769b7
makstraw <makstraw@gmail.com>
2017-12-18 16:01:54 +0300

    + Added TypedArrayExtensions for Uint8Array, toUint32Be and toUint32Le

---
+++ Friday.Base/Extensions/TypedArrayExtensions.ts
@@ -0,0 +1,11 @@
+﻿interface Uint8Array {
+    toUint32Le(): number;
+    toUint32Be(): number;
+}
+
+Uint8Array.prototype.toUint32Be = function (): number {
+    return this[3] + this[2] * 256 + this[1] * 256 * 256 + this[0] * 256 * 256 * 256;
+}
+Uint8Array.prototype.toUint32Le = function (): number {
+    return this[0] + this[1] * 256 + this[2] * 256 * 256 + this[3] * 256 * 256 * 256;
+}
