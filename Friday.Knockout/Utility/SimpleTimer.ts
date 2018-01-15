91e6868a0dd33a6c1068eecad4fc2639fa015c99
makstraw <makstraw@gmail.com>
2018-01-15 15:52:30 +0300

    + Added SimpleTimer to Friday.Knockout

---
+++ Friday.Knockout/Utility/SimpleTimer.ts
@@ -0,0 +1,28 @@
+﻿namespace Friday.Knockout.Utility {
+    export class SimpleTimer {
+        private timerValue: KnockoutObservable<number>;
+        private intervalHandle: number;
+
+        constructor(timerValuePointer: KnockoutObservable<number>) {
+            this.timerValue = timerValuePointer;
+        }
+
+        public Set(value: number) {
+            this.timerValue(value);
+        }
+
+        public Start() {
+            this.intervalHandle = setInterval(this.timerTick.bind(this), 1000);
+        }
+
+        public Stop() {
+            clearInterval(this.intervalHandle);
+        }
+
+        private timerTick() {
+            if (this.timerValue() == 0) this.Stop();
+            else
+                this.timerValue(this.timerValue() - 1);
+        }
+    }
+}
