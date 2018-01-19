9a6191d912904bc9f586495b03ba0b62093ac0d2
makstraw <makstraw@gmail.com>
2018-01-19 15:29:34 +0300

    + Added Object.Equals to compare two objects by it properties values + Added Knockout ObservableArray RemoveDeleted, AddNew, UpdateRecords to update records in array, using new array, without deleting whole array first.

---
+++ Friday.Base/Extensions/ObjectExtensions.ts
@@ -0,0 +1,28 @@
+﻿interface Object {
+    Equals(compareTo: object): boolean;
+}
+
+Object.prototype.Equals = function (compareTo: object): boolean {
+    var aProps = Object.getOwnPropertyNames(this);
+    var bProps = Object.getOwnPropertyNames(compareTo);
+
+    // If number of properties is different,
+    // objects are not equivalent
+    if (aProps.length != bProps.length) {
+        return false;
+    }
+
+    for (var i = 0; i < aProps.length; i++) {
+        var propName = aProps[i];
+
+        // If values of same property are not equal,
+        // objects are not equivalent
+        if (this[propName] !== compareTo[propName]) {
+            return false;
+        }
+    }
+
+    // If we made it this far, objects
+    // are considered equivalent
+    return true;
+}
---
+++ Friday.Knockout/Extensions/ObservableArrayExtensions.ts
@@ -0,0 +1,40 @@
+﻿interface KnockoutObservableArray<T> {
+    RemoveDeleted(records: Array<T>): void;
+    AddNew(records: Array<T>, unshift?: boolean): void;
+    UpdateRecords(records: Array<T>, unshift?: boolean): void;
+}
+
+ko.observableArray.fn.RemoveDeleted = function (records: Array<any>) {
+    this().forEach(function (item: any) {
+        if (!records.find(function (element: any): boolean {
+                if (typeof element == "object")
+                    return this.Equals(element);
+                else return this == element;
+            },
+            item)) {
+            this.remove((i: any) => {return item.Equals(i)});
+        };
+    });
+}
+
+ko.observableArray.fn.AddNew = function (records: Array<any>, unshift?: boolean) {
+    if (!unshift) unshift = false;
+
+    records.forEach(function(item: any) {
+        if(!this().find(function(element: any): boolean {
+            if (typeof element == "object")
+                return this.Equals(element);
+            else return this == element;
+        }, item)) {
+            if (unshift) this.unshift(item);
+            else this.push(item);
+        }
+    }, this);
+}
+
+ko.observableArray.fn.UpdateRecords = function (records: Array<any>, unshift?: boolean) {
+    if (!unshift) unshift = false;
+
+    this.RemoveDeleted(records);
+    this.AddNew(records, unshift);
+}
