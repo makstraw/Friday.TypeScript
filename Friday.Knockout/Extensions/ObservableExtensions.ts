23584901ea9994675b2582eec70b161b70c2e615
makstraw <makstraw@gmail.com>
2018-01-26 16:28:20 +0300

    + Added Reset method to KnockoutObservable and Default to KnockoutExtenders

---
+++ Friday.Knockout/Extensions/ObservableExtensions.ts
@@ -0,0 +1,24 @@
+﻿interface KnockoutObservable<T> {
+    Reset()
+}
+
+interface KnockoutExtenders {
+    Default(value: any);
+}
+
+ko.observable.fn.Reset = function () {
+    this(typeof this._default === 'function' ? this._default() : this._default);
+}
+
+ko.extenders.Default = function(value: any) {
+    if (typeof value === 'undefined') return;
+    this._default = value;
+
+    if (typeof this() === 'undefined') {
+        this.reset();
+    }
+
+    this.subscribe(function (newValue) {
+        typeof newValue === 'undefined' && this.reset();
+    });
+}
