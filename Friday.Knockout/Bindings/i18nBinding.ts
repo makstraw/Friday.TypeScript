dc36445f927f0fa41ba4c3b3c93b2b2e313c2f2d
makstraw <makstraw@gmail.com>
2018-02-08 13:34:04 +0300

    + Added KnockoutObservableArray.FindRecordByProperty * Changed i18n binding handler to accept object with key, params[], attr and html fields. Now i18n can localize any attribute, not only inner text, can use multiple 1 or more placeholders and can be treated as html

--- Friday.Knockout/Bindings/i18nBinding.ts
+++ Friday.Knockout/Bindings/i18nBinding.ts
@@ -2,8 +2,29 @@
     init: function(element, valueAccessor) {
 
     },
-    update: function(element, valueAccessor) {
-        var value: string = $.i18n(<string>valueAccessor());
-        $(element).text(value);
+    update: function (element, valueAccessor) {
+        let output: string;
+        let value = ko.unwrap(valueAccessor());
+        if (typeof value == "string") {
+            output = $.i18n(value);
+            $(element).text(output);
+        }
+        else {
+            console.log(value);
+            if (typeof value.params == "string" || typeof value.params == "number")
+                output = $.i18n(value.key as string, value.params);
+            else if (typeof value.params == "object" && Array.isArray(value.params)) {
+                output = $.i18n.apply(null, [value.key, ...value.params]);
+            }
+            else if (typeof value.params == "undefined")
+                output = $.i18n(value.key as string);
+
+            if (typeof value.attr == "string") {
+                $(element).attr(value.attr, output);
+            } else {
+                if (typeof value.html == "boolean" && value.html === true) $(element).html(output);
+                else $(element).text(output);
+            }
+        }
     }
 }
