16fcaa07729bac74d8141fc9ba184493b6433f5e
makstraw <makstraw@gmail.com>
2018-03-05 20:40:51 +0300

    + Added WindowTitleViewModel

---
+++ Friday.Knockout/ViewModels/WindowTitleViewModel.ts
@@ -0,0 +1,15 @@
+﻿/// <reference path="RoutedViewModel.ts" />
+module Friday.Knockout.ViewModels {
+    import IPacketRegistryRouteRegistration = Transport.IPacketRegistryRouteRegistration;
+    import IMessageSend = Transport.IMessageSend;
+
+    export abstract class WindowTitleViewModel extends RoutedViewModel {
+        public readonly Title: KnockoutObservable<string>;
+
+        constructor(initialValue: string, transport: IMessageSend, registry: IPacketRegistryRouteRegistration) {
+            super(transport, registry);
+            this.Title = ko.observable(initialValue);
+            this.Title.subscribe(newTitle => document.title = newTitle);
+        }
+    }
+}
