185da274390cc3672cce6045a016b6f59fb96b36
makstraw <makstraw@gmail.com>
2018-03-24 19:28:14 +0300

    + Added Uint8Array.IndexOfSequence method

---
+++ Friday.Base/Extensions/Uint8ArrayExtensions.ts
@@ -0,0 +1,21 @@
+﻿interface Uint8Array {
+    IndexOfSequence(searchElements: Uint8Array, fromIndex?: number): number;
+}
+
+Uint8Array.prototype.IndexOfSequence = function (this: Uint8Array, searchElements: Uint8Array, fromIndex?: number): number {
+    fromIndex = fromIndex || 0;
+
+    var index = Array.prototype.indexOf.call(this, searchElements[0], fromIndex);
+    if (searchElements.length === 1 || index === -1) {
+        // Not found or no other elements to check
+        return index;
+    }
+
+    for (var i = index, j = 0; j < searchElements.length && i < this.length; i++ , j++) {
+        if (this[i] !== searchElements[j]) {
+            return this.IndexOfSequence(searchElements, index + 1);
+        }
+    }
+
+    return (i === index + searchElements.length) ? index : -1;
+};
