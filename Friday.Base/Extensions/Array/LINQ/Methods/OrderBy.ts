57a40ddcdc4a9aa3b52f393f2db251228301a1d6
makstraw <makstraw@gmail.com>
2018-04-10 19:35:25 +0300

    F Fixed OrderBy typing

--- Friday.Base/Extensions/Array/LINQ/Methods/OrderBy.ts
+++ Friday.Base/Extensions/Array/LINQ/Methods/OrderBy.ts
@@ -1,16 +1,16 @@
 ﻿///<reference path="../../../../System/Interfaces/IComparable.ts"/>
 interface Array<T> {
-    OrderBy<TKey extends Friday.System.IComparable<TKey> | number | string>(keySelector: Selector<T, TKey>): Array<T>;
+    OrderBy<TKey extends (Friday.System.IComparable<TKey> | number | string)>(keySelector: Selector<T, TKey>): Array<T>;
     OrderUsing(comparison: Comparison<T>): Array<T>;
     OrderUsingReversed(comparison: Comparison<T>): Array<T>;
     OrderByDescending<TKey extends Friday.System.IComparable<TKey> | number | string>(keySelector: Selector<T, TKey>): Array<T>;
 }
 
 
-Array.prototype.OrderBy = function <TKey extends Friday.System.IComparable<any> | number | string>(keySelector: Selector<any, Friday.System.IComparable<TKey>>): Array<any> {
+Array.prototype.OrderBy = function <TKey extends (Friday.System.IComparable<any> | number | string)>(keySelector: Selector<any, TKey>): Array<any> {
     return this.sort((a: Friday.System.IComparable<any>, b: Friday.System.IComparable<any>) => {
-        let aKey = keySelector(a);
-        let bKey = keySelector(b);
+        let aKey = keySelector(a) as any;
+        let bKey = keySelector(b) as any;
         if (Friday.System.IsComparable(aKey)) return Friday.System.CompareTo(aKey as Friday.System.IComparable<TKey>, bKey as Friday.System.IComparable<TKey>);
         if (typeof aKey === "number") {
             if (aKey > bKey) return 1;
