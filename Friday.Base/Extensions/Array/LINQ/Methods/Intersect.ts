418714fe9a6e44da8f523c7e5a2710ae958256d0
makstraw <makstraw@gmail.com>
2018-06-29 16:59:03 +0300

    + Added Array extension LINQ.Intersect

---
+++ Friday.Base/Extensions/Array/LINQ/Methods/Intersect.ts
@@ -0,0 +1,20 @@
+﻿///<reference path="../../../../Exceptions/Basic/ArgumentException.ts"/>
+
+interface Array<T> {
+    Intersect<TResult>(second: Array<TResult>): Array<TResult>;
+//    Intersect<TResult>(second: Array<TResult>, compareSelector: EqualityComparison<TResult>): Array<TResult>;
+}
+
+Array.prototype.Intersect = function (second: Array<any>, compareSelector?: EqualityComparison<any>): Array<any> {
+    if (!Array.isArray(second)) throw new Friday.Exceptions.ArgumentException('second');
+    let output: Array<any> = [];
+    if (typeof compareSelector !== "function") {
+        compareSelector = (a, b) => a === b;
+    }
+
+
+    for (let i = 0; i < this.length; i++) {
+        if (second.Has(this[i])) output.push(this[i]);
+    }
+    return output;
+}
