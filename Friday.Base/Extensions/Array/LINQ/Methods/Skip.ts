567699147c61e222be14071d9e86f47c30d4047d
makstraw <makstraw@gmail.com>
2018-07-09 22:00:46 +0300

    F Fixed Linq.Skip

--- Friday.Base/Extensions/Array/LINQ/Methods/Skip.ts
+++ Friday.Base/Extensions/Array/LINQ/Methods/Skip.ts
@@ -8,7 +8,7 @@ Array.prototype.Skip = function (count: number): Array<any> {
     if (typeof count !== "number" || count < 0 || isNaN(count)) throw new Friday.Exceptions.ArgumentException("count");
     let output: Array<any> = [];
     if (this.length > count) {
-        output = this.slice(count+1);
-    }
+        output = this.slice(count);
+    } else output = this;
     return output;
 }
