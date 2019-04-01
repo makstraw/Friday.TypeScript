6674e9f757ae96e885d983d8a3f4c9dc7463a1b5
makstraw <makstraw@gmail.com>
2019-04-01 21:13:00 +0300

    + Added DefaultPort boolean getter to window.location extension + Added RootUrl string getter to window.location  extension

--- Friday.Base/Extensions/WindowLocationExtensions.ts
+++ Friday.Base/Extensions/WindowLocationExtensions.ts
@@ -1,4 +1,5 @@
-﻿interface IQueryParam {
+﻿///<reference path="StringExtensions.ts"/>
+interface IQueryParam {
     Name: string;
     Value: string;
 }
@@ -8,14 +9,16 @@ interface Location {
     GetParam(name: string): string;
     GetParams(): Array<IQueryParam>;
     GetParamsQuery(): string;
+    DefaultPort: boolean;
+    RootUrl: string;
 }
 
 
 window.location.GetParam = (name: string): string => {
     var params = window.location.GetParams();
-    let result = "";
+    let result = String.Empty;
     params.forEach((param: IQueryParam) => {
-        if (param.Name == name) result = param.Value;
+        if (param.Name === name) result = param.Value;
     });
     return result;
 }
@@ -25,7 +28,7 @@ window.location.GetParamByIndex = (index: number): string => {
     if (params.length > index) {
         return params[index].Value;
     }
-    return "";
+    return String.Empty;
 }
 
 window.location.GetParams = (): Array<IQueryParam> => {
@@ -36,7 +39,7 @@ window.location.GetParams = (): Array<IQueryParam> => {
 
     query.split("&").forEach((value: string, index: number) => {
         var pair = value.split("=");
-        if (pair.length == 2) {
+        if (pair.length === 2) {
             result.push({ Name: pair[0], Value: pair[1] });
         }else if (pair.length == 1) {
             result.push({ Name: index.toString(), Value: pair[0] });
@@ -47,6 +50,29 @@ window.location.GetParams = (): Array<IQueryParam> => {
 
 window.location.GetParamsQuery = (): string => {
     let startIndex = window.location.href.search("\\?");
-    if (startIndex == -1) return "";
+    if (startIndex === -1) return String.Empty;
     return window.location.href.substring(window.location.href.search("\\?")+1);
 }
+
+Object.defineProperty(window.location,
+        "DefaultPort",
+        {
+            get: function DefaultPort(): boolean {
+                return (this.protocol === "http:" && this.port === 80) ||
+                    (this.protocol === "https:" && this.port === 443);
+            },
+            set: () => undefined,
+            enumerable: true
+    });
+
+Object.defineProperty(window.location,
+    "RootUrl",
+    {
+        get: function RootUrl(): string {
+            let url = `${window.location.protocol}//${window.location.hostname}`;
+            if (!window.location.DefaultPort) url += `:${window.location.port}`;            
+            return url;
+        },
+        set: () => undefined,
+        enumerable: true
+    });
